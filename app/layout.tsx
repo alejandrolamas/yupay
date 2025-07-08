a02c91593c6e4007646063c14ac3c9514deeac0e
Alejandro Lamas <alejandro.lamas@colvet.es>
2025-07-08 20:40:38 +0200

    1.1
    Added GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,41 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "YUPAY",
   description: "Transforma tu contenido digital en ingresos",
   generator: "DRO.STUDIO",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5D7QP44P3Y"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5D7QP44P3Y');
+          `}
+        </Script>
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
